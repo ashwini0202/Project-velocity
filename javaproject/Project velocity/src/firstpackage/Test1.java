32599dd4d290fda8bc5bcfe95559d9faf4695a2b
Ashwini Biradar <ashwinichambole@gmail.com>
2024-09-27 18:22:37 +0200

    Adding file

---
+++ javaproject/Project velocity/src/firstpackage/Test1.java
@@ -0,0 +1,18 @@
+package firstpackage;
+
+import java.util.ArrayList;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList <String> al = new ArrayList<>();
+        al.add("ashwini");
+        al.add("ashwini");
+        al.add("ashwini");
+
+        for (String array : al ){
+
+            System.out.println(array);
+        }
+
+    }
+}
